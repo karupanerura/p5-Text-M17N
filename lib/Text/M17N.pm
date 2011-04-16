8549f222a1029cdb4d93953b17d9c744c75b5247
karupanerura <kenta.sato.1990@gmail.com>
2011-04-17 02:37:32 +0900

    initial commit

---
+++ lib/Text/M17N.pm
@@ -0,0 +1,32 @@
+package Text::M17N;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Text::M17N -
+
+=head1 SYNOPSIS
+
+  use Text::M17N;
+
+=head1 DESCRIPTION
+
+Text::M17N is
+
+=head1 AUTHOR
+
+Kenta Sato E<lt>kenta.sato.1990@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
